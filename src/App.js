8e4a3a305e490480f41915ce9ea99b852ff2f5fb
jat9n <jatinswami1999@gmail.com>
2024-10-19 17:03:16 +0530

    feat: state management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const add = (str) => {};
+
+  const handleChange = (e) => setInput(e.target.value);
+
+  const handleCalculate = () => {};
+
   return (
     <div className="App">
-      <div className= 'centered-container'>
-        <textarea rows={4} cols={70} spellCheck={false} />
-        <button> CALCULATE </button>
+      <div className="centered-container">
+        <textarea
+          rows={4}
+          cols={70}
+          spellCheck={false}
+          onChange={handleChange}
+        />
+        <button onClick={handleCalculate}> CALCULATE </button>
       </div>
+      {result && <div>{result}</div>}
     </div>
   );
 }
