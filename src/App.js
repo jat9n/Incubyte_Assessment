e54fbc396cb76f33a476e124fd4f88e848e85fb5
jat9n <jatinswami1999@gmail.com>
2024-10-19 16:52:20 +0530

    feat: basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className= 'centered-container'>
+        <textarea rows={4} cols={70} spellCheck={false} />
+        <button> CALCULATE </button>
+      </div>
     </div>
   );
 }
